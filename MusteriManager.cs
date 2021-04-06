5a7eb5936954defd5b12f0750f831d1d900f6cda
Beyzanur Çilekci <beyzanurcilekci@gmail.com>
2021-04-06 11:19:14 +0300

    ClassMethodDemo

---
+++ MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " Müşterisi Eklendi");
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " " + musteri.Mail);
+            }
+        }
+
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " Müşterisi Silindi");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAdi = "Beyzanur";
+            musteri1.MusteriSoyadi = "Çilekci";
+            musteri1.Mail= "bc@gmail.com";
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAdi = "Zeynep";
+            musteri2.MusteriSoyadi = "Çilekci";
+            musteri2.Mail = "zc@gmail.com";
+
+            Musteri[] musteriler = { musteri1, musteri2 };
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            Console.WriteLine("Ekleme İşlemi Simülasyonu");
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Listeleme İşlemi Simülasyonu");
+            musteriManager.Listele(musteriler);
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Silme İşlemi Simülasyonu");
+            musteriManager.Silme(musteri2);
+
+
+            Console.ReadKey();
+        }
+    }
+}
